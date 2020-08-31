3b10357a3ed033ace3a6b3e0067d35200f71f512
Talinee <bieteuy@gmail.com>
2020-09-01 00:04:26 +0700

    edit

--- app/Controllers/Http/EnrollmentController.js
+++ app/Controllers/Http/EnrollmentController.js
@@ -1,6 +1,39 @@
 'use strict'
-
-class EnrollmentController {
-}
+function numberTypeParamValidator(number) {
+    if(Number.isNaN(parseInt(number))) 
+        return {error:"param: ${number} is not support please input number" }
+    
+    return {}
+}    class EnrollmentController {
+    
+        async index() {
+            const enrollments = await Database.table('enrollments')
+            return enrollments
+        }
+        async show ({request}) {
+            const {id} = request.params
+    
+            const validatedValue = numberTypeParamValidator(id)
+    
+            if (validatedValue.error) return {
+                status : 500,error:validatedValue.error, data:undefined
+            }
+    
+            const enrollment = await Database.select("*").from("enrollments").where("enrollment_id", id).first()
+            return {status:200,error:undefined,data:enrollment || {}}
+        }
+    
+        async store ({request}) {
+            const {mark,mark_date,student_id,subject_id} = request.body
+            const missingKeys = []
+            if(!student_id) (missingKeys.push("student_id"))
+            if(!subject_id) (missingKeys.push("subject_id"))
+            if(missingKeys.lenght)
+                return {status:422,error: `${missingKeys} is missing` , data:undefined}
+            const enrollment = await Database.table("enrollments").insert({mark,mark_date,student_id,subject_id})
+            return {status:200,error:undefined,data:{mark,mark_date,student_id,subject_id}}
+        }
+    
+    }
 
 module.exports = EnrollmentController
--- app/Controllers/Http/GroupController.js
+++ app/Controllers/Http/GroupController.js
@@ -8,43 +8,35 @@ function numberTypeParamValidator(number){
     return {status:200, error:undefined, data:teacher}
     return {}
 }
+
 class GroupController {
     async index() {
-        const groups =  await Database.table('gruops')    
- 
-        return {status:200 ,error:undefined,data: groups }
-     }  
-     async show ({request}) {
-         const {id} = request.params
-         const validatedValue = numberTypeParamValidator(id)
- 
-         if (validatedValue.error) 
-         return { status:500, error: validatedValue.error, data: undefined }
-         const gruop = await Database
-         .select('*')
-         .from('groups')
-         .where("group_id",id)
-         .first()
- 
-         return {status:200 ,error:undefined,data:groups|| {}}
-     }
-     async store ({request}){
-         const {name} = request.body
- 
-         const missingKeys = []
- 
-         if(!name ) missingKeys.push('name')
- 
-         if (missingKeys.length)
-         return { status:422, error:`${missingKeys} is missing`, data: undefined }
+        const groups = await Database.table('groups')
+        return groups
+    }
+    async show ({request}) {
+        const {id} = request.params
 
- 
-        const group = await Database
-        .table('groups')
-        .insert({name})
- 
-        return {status:200,error: undefined,data :name}
-     }
+        const validatedValue = numberTypeParamValidator(id)
+
+        if (validatedValue.error) return {
+            status : 500,error:validatedValue.error, data:undefined
+        }
+
+        const subject = await Database.select("*").from("groups").where("group_id", id).first()
+        return {status:200,error:undefined,data:group || {}}
+    }
+
+    async store ({request}) {
+        const {name} = request.body
+        const missingKeys = []
+        if(!name) (missingKeys.push("name"))
+        if(missingKeys.lenght)
+            return {status:422,error: `${missingKeys} is missing` , data:undefined}
+        const group = await Database.table("groups").insert({name})
+        return {status:200,error:undefined,data:{name}}
+    }
+
 }
 
 module.exports = GroupController
